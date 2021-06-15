f5fca57c402aed13494288a803f564136cbd6f3d
JulCzar <julczar21@outlook.com>
2021-06-15 01:12:02 -0300

    finalização do dia anterior

--- src/main/java/br/czar/odonto/controller/ProfileController.java
+++ src/main/java/br/czar/odonto/controller/ProfileController.java
@@ -1,15 +1,23 @@
 package br.czar.odonto.controller;
 
+import br.czar.odonto.aplication.storage.SessionStorage;
+import br.czar.odonto.model.Allergy;
+import br.czar.odonto.model.Patient;
+
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Named
 @ViewScoped
 public class ProfileController implements Serializable {
 	@Serial
 	private static final long serialVersionUID = 2122937466427799647L;
+	private static final String LOGGED_USER = "logged-user";
 	private boolean editing;
 
 	public ProfileController() {
@@ -29,5 +37,12 @@ public class ProfileController implements Serializable {
 	}
 	public void disableEdit() {
 		setEditing(false);
+	}
+
+	public List<String> getAllergies() {
+		Object o = SessionStorage.getItem(LOGGED_USER);
+		if (o == null) return new ArrayList<>();
+		if (o instanceof Patient) return ((Patient)o).getAllergies().stream().map(Allergy::getName).collect(Collectors.toList());
+		return new ArrayList<>();
 	}
 }
--- src/main/java/br/czar/odonto/model/Security.java
+++ src/main/java/br/czar/odonto/model/Security.java
@@ -1,10 +1,8 @@
 package br.czar.odonto.model;
 
-import br.czar.odonto.aplication.Util;
 import org.apache.commons.codec.digest.DigestUtils;
 
 public class Security {
-	private Security() {}
 	public static PhysicalPerson encript(PhysicalPerson p) {
 		String last3 = p.getPassword().substring(p.getPassword().length() - 3);
 		String encripted = DigestUtils.sha256Hex(p.getPassword() + last3);
